6f285f26c18b926f7a5cb5f1ea3228b924bf76af
Ava Chow <github@achow101.com>
2025-05-20 16:53:42 -0700

    Split base58 into _base58

--- src/wallet_manipulator/__init__.py
+++ src/wallet_manipulator/__init__.py
@@ -16,6 +16,7 @@ from typing import (
     List,
 )
 
+from ._base58 import b58_check_encode
 from ._db import (
     get_bdb_cursor,
     get_sqlite_cursor,
@@ -40,42 +41,6 @@ def output_type_to_str(i: int) -> str:
         return "bech32m"
     else:
         return "unknown"
-
-
-def b58_encode(b: bytes) -> str:
-    """
-    Encode bytes to a base58-encoded string
-
-    :param b: Bytes to encode
-    :return: Base58 encoded string of ``b``
-    """
-    b58_digits: str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
-
-    # Convert big-endian bytes to integer
-    n: int = int.from_bytes(b"\x00" + b, byteorder="big")
-
-    # Divide that integer into base58
-    temp: List[str] = []
-    while n > 0:
-        n, r = divmod(n, 58)
-        temp.append(b58_digits[r])
-    res: str = "".join(temp[::-1])
-
-    # Encode leading zeros as base58 zeros
-    czero: int = 0
-    pad: int = 0
-    for c in b:
-        if c == czero:
-            pad += 1
-        else:
-            break
-    return b58_digits[0] * pad + res
-
-
-def b58_check_encode(b: bytes) -> str:
-    checksum = hashlib.sha256(hashlib.sha256(b).digest()).digest()[0:4]
-    data = b + checksum
-    return b58_encode(data)
 
 
 def dump(args):
